9e669796aeb0a882f056a1814f1251ef4f33ac3a
PleBea <bini060315kim@gmail.com>
2024-12-13 16:55:33 +0900

    feat: update import path for Index component to lowercase 'index'

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 import "./assets/styles/index.css";
-import Index from "./pages/Index";
+import Index from "./pages/index";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
